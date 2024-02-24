e53a49a00461aa311093bb4d1f2bd05fc5cdab55
Nasif <nasifuad007@gmail.com>
2024-02-24 22:02:04 +0600

    changed html file name

--- calculator.js
+++ calculator.js
@@ -1,24 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const display = document.getElementById("display");
-
-//     window.display_button = function(input){
-//         display.value += input;
-//     };
-
-//     window.clear = function(){
-//         display.value = "";
-//     };
-
-//     window.calculate = function(){
-//         try{
-//             display.value = eval(display.value);
-//         }
-//         catch(error){
-//             display.value = "Error";
-//         }
-//     };
-// });
-
 function display_button(input){
     let x= input
     
